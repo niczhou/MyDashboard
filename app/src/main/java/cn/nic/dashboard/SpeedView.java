672d80b4666734cb28d72d1ac4548e360dfa356d
niczhou <madeozhou@sina.com>
2017-11-30 09:13:52 +0800

    on onmeasure

--- app/src/main/java/cn/nic/dashboard/SpeedView.java
+++ app/src/main/java/cn/nic/dashboard/SpeedView.java
@@ -170,8 +170,8 @@ public class SpeedView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         float ratio=2.43f;
-        int widthMeasured=0;
-        int heightMeasured=0;
+        int widthMeasured=222;
+        int heightMeasured=222;
 
         // 父容器传过来的宽度方向上的模式
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
@@ -189,19 +189,19 @@ public class SpeedView extends View {
                 && heightMode == MeasureSpec.EXACTLY ) {
             // 判断条件为，宽度模式为Exactly，也就是填充父窗体或者是指定宽度；
             // 且高度模式不是Exaclty，代表设置的既不是fill_parent也不是具体的值，于是需要具体测量
-            // 且图片的宽高比已经赋值完毕，不再是0.0f
-            // 表示宽度确定，要测量高度
-            widthMeasured=MeasureSpec.makeMeasureSpec(widthSize,MeasureSpec.EXACTLY);
-            heightMeasured=MeasureSpec.makeMeasureSpec(heightSize,MeasureSpec.EXACTLY);
+//            widthMeasured=MeasureSpec.makeMeasureSpec(widthSize,MeasureSpec.EXACTLY);
+//            heightMeasured=MeasureSpec.makeMeasureSpec(heightSize,MeasureSpec.EXACTLY);
+            widthMeasured=widthSize;
+            heightMeasured=heightSize;
 //            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height,MeasureSpec.EXACTLY);
-        } else if (widthMode != MeasureSpec.EXACTLY
-                && heightMode == MeasureSpec.EXACTLY && ratio != 0.0f) {
-//            // 判断条件跟上面的相反，宽度方向和高度方向的条件互换
-//            // 表示高度确定，要测量宽度
-//            width = (int) (height * ratio + 0.5f);
-//
-//            widthMeasureSpec = MeasureSpec.makeMeasureSpec(width,
-//                    MeasureSpec.EXACTLY);
+//        } else if (widthMode != MeasureSpec.EXACTLY
+//                && heightMode == MeasureSpec.EXACTLY && ratio != 0.0f) {
+////            // 判断条件跟上面的相反，宽度方向和高度方向的条件互换
+////            // 表示高度确定，要测量宽度
+////            width = (int) (height * ratio + 0.5f);
+////
+////            widthMeasureSpec = MeasureSpec.makeMeasureSpec(width,
+////                    MeasureSpec.EXACTLY);
         }
 
         super.onMeasure(widthMeasured, heightMeasured);
